bad42c8e8d64013b1acb87cb38eca7e6cc8684e8
Anhad <84476084+07anhad@users.noreply.github.com>
2022-07-09 16:29:26 +0530

    displaying exercise names

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -16,14 +16,17 @@ const ExerciseCard = ({ exercise }) => {
     return (
         <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
           <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"></img>
-          <Stack direction='row'>
-              <Button sx={{ ml: '21px', color: 'white', background: '#00645f82', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize',  }}>
-                  {exercise.bodyPart}
-              </Button>
-              <Button sx={{ ml: '21px', color: 'white', background: '#00645f82', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize',  }}>
+          <Stack direction='row' >
+                <Button sx={{ ml: '21px', color: 'white', background: '#00645f82', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize',  }}>
+                    {exercise.bodyPart}
+                </Button>
+                <Button sx={{ ml: '21px', color: 'white', background: '#00645f82', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize',  }}>
                   {exercise.target}
-              </Button>
-          </Stack>
+                </Button>
+            </Stack>
+            <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize" fontSize="18px">
+              {exercise.name}
+            </Typography>
         </Link>
   )
 }
